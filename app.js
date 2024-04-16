d2088bd83feeaeb484172e91a4b1ad56cb114302
Jacob <jacobrashid99@gmail.com>
2024-04-16 16:41:29 -0700

    started fetch implementation WIP

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Let's get this party started!");
+
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+async function searchForGif(){
+  const search = document.querySelector('#gif-search').value;
+  
+  const response = await fetch(
+    `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`
+  );
+  const gif = await response.json();
+  console.log(gif);
+}
