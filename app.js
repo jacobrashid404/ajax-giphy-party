ae4dede2823ee51d1c099f9ee065dccfd09bccbc
Jacob <jacobrashid99@gmail.com>
2024-04-16 17:24:12 -0700

    implement appending random gif to dom, WIP

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 console.log("Let's get this party started!");
 
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
-let search = document.querySelector('#gif-search');
+let $search = document.querySelector('#gif-search');
+let $gifContainer = document.querySelector('#gif-container');
 
 async function searchForGif() {
 
   const response = await fetch(
-    `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`
+    `http://api.giphy.com/v1/gifs/search?q=${$search.value}&api_key=${API_KEY}`
   );
   const gif = await response.json();
-  console.log(gif);
   return gif;
 }
 
 async function handleClick(evt) {
   evt.preventDefault();
 
-  const searchValue = search.value;
-
   const giphyData = await searchForGif();
   console.log(giphyData);
+  const randGifIndex = pickRandIndex(giphyData.data);
+  addGif(giphyData.data[randGifIndex].url);
+}
 
+function pickRandIndex(gifs){
+  console.log('pickRandIndex', gifs);
+  const gifDataLength = gifs.length;
+  return Math.floor(Math.random() * gifDataLength);
+}
+
+function addGif(gifURL){
+  //append to gif div
+  const $newGif = document.createElement("img");
+  $newGif.setAttribute('src', gifURL);
+  $gifContainer.append($newGif);
 }
 
 
