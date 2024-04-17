5d1ee65b4d8e04c46c2266f698e58bd04b360188
Zach Bunting <zvrbunting@gmail.com>
2024-04-16 18:11:21 -0700

    Post code review.

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ console.log("Let's get this party started!");
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 const $gifForm = document.querySelector(".gif-form");
 const $removeGIFsButton = document.querySelector(".remove-button");
-let $search = document.querySelector('#gif-search');
-let $gifContainer = document.querySelector('#gif-container');
+const $search = document.querySelector('#gif-search');
+const $gifContainer = document.querySelector('#gif-container');
 
 $gifForm.addEventListener("submit", handleClick);
 $removeGIFsButton.addEventListener('click', removeGIFs);
+
+//TODO: rather than embed the search.value, make the searchForGif get
+// called with a parameter url
+//TODO: URLSearchParams
+//TODO: be mindful of how you're naming variables
+//TODO: don't have super long return statements
 
 /** search the Giphy website for a gif based on user input */
 
@@ -20,6 +26,9 @@ async function searchForGif() {
   return gif;
 }
 
+//TODO: make sure docstring says what the function is doing
+//TODO: can pass the searchForGif the url
+//TODO: handle submit is a better name (using submit not click)
 /** conductor function that handles form input */
 
 async function handleClick(evt) {
@@ -30,6 +39,12 @@ async function handleClick(evt) {
   const randGifIndex = pickRandIndex(giphyData.data);
   addGif(giphyData.data[randGifIndex].images.original.url);
 }
+
+//TODO: can put the event listeners close to the handler itself
+// follow the convention
+
+//TODO: think about making functions more generic (just getting a random
+// number, unrelated to gifs)
 
 /** selects a random number based on the amount of gifs returned from
  * searchForGif
