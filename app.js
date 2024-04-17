1911b576616d52bc7dae1f5da39baa5cacd06ba9
Zach Bunting <zvrbunting@gmail.com>
2024-04-16 17:01:52 -0700

    Updated handleClick.

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 console.log("Let's get this party started!");
 
 const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+let search = document.querySelector('#gif-search');
 
-async function searchForGif(){
-  const search = document.querySelector('#gif-search').value;
-  
+async function searchForGif() {
+
   const response = await fetch(
     `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`
   );
   const gif = await response.json();
   console.log(gif);
+  return gif;
 }
+
+async function handleClick(evt) {
+  evt.preventDefault();
+
+  const searchValue = search.value;
+
+  const giphyData = await searchForGif();
+  console.log(giphyData);
+
+}
+
+
+const $gifForm = document.querySelector(".gif-form");
+$gifForm.addEventListener("submit", handleClick);
